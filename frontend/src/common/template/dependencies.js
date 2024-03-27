afa87ded80966ea67fde2272f491217d77a2fce2
Christian Valentim <christianvalentim99@gmail.com>
2024-03-27 17:48:02 -0300

    atualização da pasta dependecies

--- frontend/src/common/template/dependencies.js
+++ frontend/src/common/template/dependencies.js
@@ -1,17 +1,16 @@
 const jquery = require('jquery');
 window.$ = window.jQuery = jquery;
  
-require('modules/admin-lte/dist/js/app');
-require('modules/admin-lte/plugins/jQueryUI/jquery-ui.min');
-require('modules/admin-lte/bootstrap/js/bootstrap.min');
-require('modules/admin-lte/plugins/fastclick/fastclick');
-require('modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min');
+import 'modules/admin-lte/plugins/jQueryUI/jquery-ui.min'
+import 'modules/admin-lte/plugins/fastclick/fastclick'
+import 'modules/admin-lte/plugins/slimScroll/jquery.slimscroll.min'
+import 'modules/admin-lte/dist/js/app.min'
  
-require('modules/font-awesome/css/font-awesome.min.css');
-require('modules/ionicons/dist/css/ionicons.css');
-require('modules/admin-lte/bootstrap/css/bootstrap.min.css');
-require('modules/admin-lte/dist/css/AdminLTE.css');
-require('modules/admin-lte/dist/css/skins/_all-skins.css');
-require('modules/admin-lte/plugins/iCheck/flat/blue.css');
+import 'modules/font-awesome/css/font-awesome.min.css'
+import 'modules/ionicons/dist/css/ionicons.min.css'
+import 'modules/admin-lte/bootstrap/css/bootstrap.min.css'
+import 'modules/admin-lte/dist/css/AdminLTE.min.css'
+import 'modules/admin-lte/dist/css/skins/_all-skins.min.css'
+import 'modules/admin-lte/plugins/iCheck/flat/blue.css'
  
-require('./custom.css');
+import './custom.css'
