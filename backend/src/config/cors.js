d5dc38c02cccc16deb4c12258df6422257967fe2
Christian Valentim <christianvalentim99@gmail.com>
2024-03-27 22:17:17 -0300

    Autenticação do backend

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,16 +1,8 @@
-module.exports = () => {
-  const express = require('express');
-  const cors = require('cors');
-  const app = express();
-  
-  // Habilitar o CORS para todas as origens (isso pode ser ajustado conforme necessário)
-  app.use(cors());
-  
-  // Resto da sua configuração do servidor
-  // ...
-  
-  app.listen(3003, () => {
-    console.log('Servidor em execução na porta 27017');
-  });
-  
+module.exports = (req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods',
+  'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization')
+  next()
 }
