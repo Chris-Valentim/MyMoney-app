f330d6e01b82a3e5e4e61c4b2c23b29023e3c2d8
Christian Valentim <christianvalentim99@gmail.com>
2024-06-04 20:09:08 -0300

    ajuste na formatação da estrutura do código

--- frontend/src/common/form/labelAndInput.jsx
+++ frontend/src/common/form/labelAndInput.jsx
@@ -5,9 +5,13 @@ export default props => (
   <Grid cols={props.cols}>
     <div className='form-group'>
       <label htmlFor={props.name}>{props.label}</label>
-      <input {...props.input} className='form-control'
+      <input
+        {...props.input}
+        className='form-control'
         placeholder={props.placeholder}
-        readOnly={props.readOnly} type={props.type} />
+        readOnly={props.readOnly}
+        type={props.type}
+      />
     </div>
   </Grid>
 )
